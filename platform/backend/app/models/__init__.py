a203818e785ae20a0d187a2401a1784dd5fd8040
Shahbaaz Ahmed <shahbaaz42@gmail.com>
2026-09-12 15:06:42 +0530

    Export foundation models

---
+++ platform/backend/app/models/__init__.py
@@ -0,0 +1,17 @@
+from .foundation import (
+    AcademicDivision,
+    AcademicDivisionGradeLevel,
+    AcademicYear,
+    ClassGroup,
+    GradeLevel,
+    Institution,
+)
+
+__all__ = [
+    "Institution",
+    "AcademicYear",
+    "AcademicDivision",
+    "GradeLevel",
+    "AcademicDivisionGradeLevel",
+    "ClassGroup",
+]
