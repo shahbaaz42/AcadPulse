eae517efe48055456a90ddb3e1edd7073b58602e
Shahbaaz Ahmed <shahbaaz42@gmail.com>
2026-09-13 14:27:22 +0530

    Expose organization router

--- platform/backend/app/routers/__init__.py
+++ platform/backend/app/routers/__init__.py
@@ -1,3 +1,4 @@
 from .foundation import router as foundation_router
+from .organization import router as organization_router
 
-__all__ = ["foundation_router"]
+__all__ = ["foundation_router", "organization_router"]
